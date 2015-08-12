2ea985c9cff07915fd22967a9ff7e3d2fe08f2f4
jgarcia162 <jogarcia809@yahoo.com>
2015-08-11 21:39:09 -0400

    Testing Firebase

--- app/src/main/java/com/example/c4q_ac35/espy/EspyMain.java
+++ app/src/main/java/com/example/c4q_ac35/espy/EspyMain.java
@@ -4,21 +4,47 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
 
 public class EspyMain extends ActionBarActivity {
 
-//   Client id
-//    GHO15NRJ1DFJECCEPOPOC555Y1MKI23LPQQZHG04F2AG3FPJ
-
-//    Client secret
-//    4CV4XEO03BPPLXSMOFVOB4KG14SSKQYGH20X3VN1RM5RLBRY
+    TextView mTextView,mTesting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_espy_main);
+
+        mTextView = (TextView) findViewById(R.id.textView);
+        mTesting = (TextView) findViewById(R.id.testing);
+
+
+        // TESTING FIREBASE CONNECTION
+        Firebase.setAndroidContext(this);
+
+        Firebase myFireBase = new Firebase("https://blinding-torch-1145.firebaseio.com/");
+
+        myFireBase.child("message").setValue("Do you have data, you'll love Firebase!");
+
+        myFireBase.child("message").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                mTesting.setText(dataSnapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
